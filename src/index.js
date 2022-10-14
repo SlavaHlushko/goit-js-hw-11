5de3fcdfa2aed22aad4b0f379f45d13fb8febd18
SlavaHlushko <slavahlushko90@gmail.com>
2022-10-14 21:06:06 +0300

    add _gallery.scss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,128 @@
 'use strict';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchImages } from './js/fetchImages';
+
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  buttonForm: document.querySelector('.button-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.load-more'),
+};
+
+const { searchQuery } = refs.searchForm;
+
+const lightBox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+const HITS_PER_PAGE = 40;
+let items = [];
+let query = '';
+let currentPage = 1;
+let totalPages = 0;
+
+// ---  Fetching data from array of images---
+async function fetchData() {
+  const data = await fetchImages(query, HITS_PER_PAGE, currentPage);
+  items = await [...items, data.hits];
+  totalPages = await data.totalHits;
+  renderList(data.hits);
+
+  if (currentPage === 1 && data.total > 0) {
+    return Notiflix.Notify.success(`Hooray! We found  ${data.total} images`, {
+      position: 'center-center',
+    });
+  }
+
+  if (data.hits.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+      {
+        position: 'center-center',
+      }
+    );
+    refs.loadMore.classList.add('hidden');
+    return;
+  }
+
+  if (currentPage > Number(totalPages / HITS_PER_PAGE)) {
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results.",
+      {
+        position: 'center-center',
+      }
+    );
+    refs.loadMore.classList.add('hidden');
+    return;
+  }
+}
+
+const handleSubmit = async event => {
+  event.preventDefault();
+  if (query === searchQuery.value) return;
+
+  query = await searchQuery.value;
+  refs.gallery.innerHTML = '';
+  currentPage = 1;
+  items = [];
+  if (!query) return;
+
+  fetchData();
+};
+
+refs.searchForm.addEventListener('submit', handleSubmit);
+
+function renderList(itemsData) {
+  refs.gallery.insertAdjacentHTML('beforeend', createMarkup(itemsData));
+  lightBox.refresh();
+  refs.loadMore.classList.remove('hidden');
+}
+
+function createMarkup(items) {
+  return items
+    .map(item => {
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = item;
+      return `
+      <div class="photo-card">
+            <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes</b> ${likes}
+                </p>
+                <p class="info-item">
+                    <b>Views</b> ${views}
+                </p>
+                <p class="info-item">
+                    <b>Comments</b> ${comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b> ${downloads}
+                </p>
+            </div>
+        </div>
+      `;
+    })
+    .join(' ');
+}
+
+// --- Load more click ---
+refs.loadMore.addEventListener('click', loadMoreClick);
+
+function loadMoreClick() {
+  currentPage += 1;
+  fetchData();
+
+  refs.loadMore.classList.add('hidden');
+}
---
+++ src/js/fetchImages.js
@@ -0,0 +1,21 @@
+'use strict';
+import axios from 'axios';
+import Notiflix from 'notiflix';
+
+const baseURL = 'https://pixabay.com/api/?key=';
+const API_KEY = '29451964-958278d8f10d2abadadf36c5e';
+const searchOption = 'image_type=photo&orientation=horizontal&safesearch=true';
+
+export async function fetchImages(searchQuery, HITS_PER_PAGE, currentPage) {
+  try {
+    const res = await axios.get(
+      `${baseURL}${API_KEY}&q=${searchQuery}&${searchOption}&per_page=${HITS_PER_PAGE}&page=${currentPage}`
+    );
+
+    return await res.data;
+  } catch (error) {
+    Notiflix.Notify.failure(error.message, {
+      position: 'center-center',
+    });
+  }
+}
